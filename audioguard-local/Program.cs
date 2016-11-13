db5343f1b4c1a101ed2b6863f8f830a7aa7ca05d
Fede <ftedinur@itba.edu.ar>
2016-11-13 00:34:52 -0300

    added project files

---
+++ audioguard-local/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace iot {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("starting");
+            CoreAudioDevice a = new CoreAudioController().DefaultPlaybackDevice;
+
+            while (true) {
+                Console.WriteLine("curent vol: " + a.Volume);
+                Console.WriteLine("input new vol:");
+                string i = Console.ReadLine();
+                int v;
+                if (!int.TryParse(i, out v)) {
+                    continue;
+                }
+
+                a.Volume = v;
+            }
+        }
+    }
+}
